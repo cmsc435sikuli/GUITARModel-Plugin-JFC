85500837c0cd23cfb13e0047c2f69d1f974d9e78
jsternberg <none@none>
2010-10-13 22:43:27 +0000

    Finishing commit for changing the id generators.
    --HG--
    extra : convert_revision : svn%3Acc9e4985-2604-4ca3-9a6b-1c23c61bd3f4/GUITARModel-Plugin-JFC/trunk%402514

--- src/edu/umd/cs/guitar/model/JFCDefaultIDGenerator.java
+++ src/edu/umd/cs/guitar/model/JFCDefaultIDGenerator.java
@@ -38,7 +38,7 @@ import edu.umd.cs.guitar.model.wrapper.GUITypeWrapper;
  * @author <a href="mailto:baonn@cs.umd.edu"> Bao N. Nguyen </a>
  * 
  */
-public class JFCDefaultIDGenerator implements GIDGenerator {
+public class JFCDefaultIDGenerator extends DefaultIDGenerator {
 
 	static JFCDefaultIDGenerator instance = null;
 	static ObjectFactory factory = new ObjectFactory();
@@ -51,168 +51,9 @@ public class JFCDefaultIDGenerator implements GIDGenerator {
 	}
 
 	private JFCDefaultIDGenerator() {
-	}
-
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see
-	 * edu.umd.cs.guitar.model.GIDGenerator#genID(edu.umd.cs.guitar.model.data
-	 * .GUIStructure)
-	 */
-	@Override
-	public void generateID(GUIStructure gs) {
-		for (GUIType gui : gs.getGUI()) {
-			generateGUIID(gui);
-		}
-	}
-
-	/**
-	 * @param gui
-	 */
-	private void generateGUIID(GUIType gui) {
-		ContainerType container = gui.getContainer();
-
-		if (container == null)
-			return;
-
-		long windowHashCode = getWindowHashCode(gui);
-
-		generateComponentID(container, windowHashCode);
-	}
-
-	/**
-	 * @param gui
-	 * @return
-	 */
-	private long getWindowHashCode(GUIType gui) {
-
-		GUITypeWrapper wGUI = new GUITypeWrapper(gui);
-		String title = wGUI.getTitle();
-		long hashcode = title.hashCode();
-
-		hashcode = (hashcode * 2) & 0xffffffffL;
-		return hashcode;
-	}
-
-	private void generateComponentID(ComponentType component,
-			long parentHashCode) {
-
-		AttributesType attributes = component.getAttributes();
-
-		long hashcode = 1;
-
-		if (attributes != null) {
-
-			long localHashCode = getLocalHashcode(component);
-			hashcode = parentHashCode * prime + localHashCode;
-			hashcode = (hashcode * 2) & 0xffffffffL;
-
-			String sID = GUITARConstants.COMPONENT_ID_PREFIX + hashcode;
-
-			PropertyType property = factory.createPropertyType();
-			property.setName(GUITARConstants.ID_TAG_NAME);
-			property.getValue().add(sID);
-
-			attributes.getProperty().add(0, property);
-		} else {
-			hashcode = parentHashCode;
-		}
-
-		if (component instanceof ContainerType) {
-			ContainerType container = (ContainerType) component;
-			List<ComponentType> children = container.getContents()
-					.getWidgetOrContainer();
-
-			boolean isAddIndex = !hasUniqueChildren(component);
-
-			for (ComponentType child : children) {
-
-				long propagatedHashCode = hashcode;
-				if (isAddIndex) {
-					propagatedHashCode = prime * propagatedHashCode
-							+ ((Integer) children.indexOf(child)).hashCode();
-				}
-				
-				generateComponentID(child, propagatedHashCode);
-			}
-		}
-	}
-
-	/**
-	 * @param component
-	 * @return
-	 */
-	private boolean hasUniqueChildren(ComponentType component) {
-		if (!(component instanceof ContainerType))
-			return true;
-		
-		List<Long> examinedHashCode = new ArrayList<Long>();
-		
-		ContainerType container = (ContainerType) component;
-		for(ComponentType child: container.getContents().getWidgetOrContainer()){
-			long hashcode = getLocalHashcode(child);
-			if(examinedHashCode.contains(hashcode)){
-				return false;
-			}else{
-				examinedHashCode.add(hashcode);
-			}
-		}
-		return true;
+		super(ID_PROPERTIES);
 	}
 
 	static List<String> ID_PROPERTIES = new ArrayList<String>(
 			JFCConstants.ID_PROPERTIES);
-
-	/**
-	 * @param component
-	 * @return
-	 */
-	private long getLocalHashcode(ComponentType component) {
-		// ComponentTypeWrapper wComponent = new
-		// ComponentTypeWrapper(component);
-		//
-		// String sClass = wComponent
-		// .getFirstValueByName(GUITARConstants.CLASS_TAG_NAME);
-		// if (IGNORED_ID_CLASSES.contains(sClass)) {
-		// return 0;
-		// }
-		//
-		// preprocessID(component);
-
-		final int prime = 31;
-
-		long hashcode = 1;
-
-		AttributesType attributes = component.getAttributes();
-		if (attributes == null)
-			return hashcode;
-
-		List<PropertyType> lProperty = attributes.getProperty();
-		if (lProperty == null)
-			return hashcode;
-
-		for (PropertyType property : lProperty) {
-			if (ID_PROPERTIES.contains(property.getName())) {
-
-				String name = property.getName();
-				hashcode = (prime * hashcode + (name == null ? 0 : name
-						.hashCode()));
-
-				List<String> valueList = property.getValue();
-				if (valueList != null)
-					for (String value : valueList) {
-						hashcode = (prime * hashcode + (value == null ? 0
-								: (value.hashCode())));
-
-					}
-
-			}
-		}
-
-		hashcode = (hashcode * 2) & 0xffffffffL;
-
-		return hashcode;
-
-	}
 }
